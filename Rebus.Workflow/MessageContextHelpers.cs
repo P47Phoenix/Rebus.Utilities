86fe7afce301ffe1aa691bb6deb38fd3ef038c14
Michael Connelly <michaelconne@gmail.com>
2018-02-10 14:13:20 -0600

    Created rebus solution for message workflow

---
+++ Rebus.Workflow/MessageContextHelpers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Rebus.Bus;
+using Rebus.Messages;
+using Rebus.Pipeline;
+using Rebus.Serialization;
+
+namespace Rebus.Workflow
+{
+    public static class MessageContextHelpers
+    {
+        private static readonly ObjectSerializer m_objectSerializer = new ObjectSerializer();
+
+        public static object Get(this IMessageContext messageContext, string key)
+        {
+            if (messageContext.Headers.ContainsKey(key) == false)
+            {
+                return null;
+            }
+
+            var messageString = messageContext.Headers[key];
+
+            return m_objectSerializer.DeserializeFromString(messageString);
+        }
+
+        public static void Set<T>(this IMessageContext messageContext, string key, T data)
+        {
+            messageContext.Headers[key] = m_objectSerializer.SerializeToString(data);
+        }
+
+        public static string GetKey(this IMessageContext messageContext)
+        {
+            var messsageId = messageContext.Message.GetMessageId();
+
+            var correlationId = messageContext.Headers[Headers.CorrelationId];
+
+            return $"{correlationId}_{messsageId}";
+        }
+    }
+}
---
+++ Rebus.Workflow/OnWorkflowItemCompleted.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Rebus.Bus;
+using Rebus.Messages;
+using Rebus.Pipeline;
+
+namespace Rebus.Workflow
+{
+    public class OnWorkflowItemCompleted : IIncomingStep
+    {
+        private readonly MessageContext m_messageContext;
+        private readonly IBus m_bus;
+
+        public OnWorkflowItemCompleted(MessageContext messageContext, IBus bus)
+        {
+            m_messageContext = messageContext;
+            m_bus = bus;
+        }
+
+        public async Task Process(IncomingStepContext context, Func<Task> next)
+        {
+            await next();
+            
+            var key = m_messageContext.GetKey();
+
+            var nextMessage = m_messageContext.Get(key);
+
+            if (nextMessage == null)
+            {
+                return;
+            }
+
+            await m_bus.Send(nextMessage, new Dictionary<string, string>()
+            {
+                {
+                    Headers.CorrelationId,
+                    m_messageContext.Headers[Headers.CorrelationId]
+                },
+                {
+                    Headers.MessageId,
+                    m_messageContext.Headers[Headers.MessageId]
+                }
+            });
+        }
+    }
+}
